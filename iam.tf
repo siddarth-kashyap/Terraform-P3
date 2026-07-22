0257908296dfe5e4fba4f2efce69c7f6e7722a14
Siddarth Kashyap <50227008+siddarth-kashyap@users.noreply.github.com>
2026-07-22 10:03:26 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -12,7 +12,7 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   })
 }
 
-# 2. Attach the Standard ECS Policy
+# 2. Attaching the Standard ECS Policy
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
