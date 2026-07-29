43f57b00a6ed0fb0de501dab418d8e3e87649d1b
Siddarth Kashyap <50227008+siddarth-kashyap@users.noreply.github.com>
2026-07-29 21:22:55 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "sid-tf-s3-bucket" # Change to a unique name
+    bucket         = "sid-tf-s3-bucket" # Change to a unique name-
     key            = "assignments/part3/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-state-locking"
