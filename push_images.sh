4b1ea13aae76f99a9a7a30b34568140ff8f5e310
Siddarth Kashyap <50227008+siddarth-kashyap@users.noreply.github.com>
2026-07-29 21:24:15 +0530

    Update push_images.sh

--- push_images.sh
+++ push_images.sh
@@ -6,7 +6,7 @@ REGION="ap-south-1"
 REPO_URL="$AWS_ID.dkr.ecr.$REGION.amazonaws.com"
 
 # 1. Login to ECR
-# This uses your 'aws configure' credentials to get a temporary password
+# This uses your 'aws configure' credential to get a temporary password
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $REPO_URL
 
 # 2. Build and Push Flask (Assuming your folder is named 'flask_app')
